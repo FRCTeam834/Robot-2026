04c211620057cf468e04a5fa9138ada0eec71b08
Akshaya Guduru <hipinkorange@gmail.com>
2026-01-28 21:47:23 -0500

    Co-authored-by: 1danskim-ai <1danskim-ai@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -21,6 +21,35 @@ public class Shooter extends SubsystemBase {
   private final SysIdRoutine flywheelSysId;
   private final SysIdRoutine hoodSysId;
 
+  //Interpolating Tables
+  private static InterpolatingDoubleTreeMap shotAngleTable = new InterpolatingDoubleTreeMap;
+  private static InterpolatingDoubleTreeMap shotSpeedTable = new InterpolatingDoubleTreeMap;
+  private static double hoodPivotAngle = Units.degreesToRadians(0.95);  // does this have to do with the interpolating table gabe?
+
+  //initializing values for shot table -- THESE #s ARE IRRELEVANT AND NOT TESTED
+  static {
+    /** key: <horizontal distance m>, value: <pivot angle rad> */
+    shotAngleTable.put(0.0, 0.75);
+    shotAngleTable.put(1.998, 0.743);
+    shotAngleTable.put(2.235, 0.7148);
+    shotAngleTable.put(2.576, 0.6854); 
+    shotAngleTable.put(2.872, 0.6213); 
+    shotAngleTable.put(3.27, 0.6);
+    shotAngleTable.put(3.508, 0.565);
+    shotAngleTable.put(3.976, 0.523);
+    shotAngleTable.put(100.0, 0.523);
+    
+    /** key: <horizontal distance m>, value: <rpm> */
+    
+    shotSpeedTable.put(0.0, 4000.0);
+    shotSpeedTable.put(1.998, 4000.0);
+    shotSpeedTable.put(2.235, 4000.0);
+    shotSpeedTable.put(3.27, 4200.0);
+    shotSpeedTable.put(3.976, 4600.0);
+    shotSpeedTable.put(100.0, 4600.0);
+    
+  }
+  
   // Shooter Constructor
   public Shooter(ShooterIO io) {
     this.io = io;
@@ -90,6 +119,20 @@ public class Shooter extends SubsystemBase {
   public Command hoodDynamicReverse() {
     return hoodSysId.dynamic(SysIdRoutine.Direction.kReverse);
   }
+
+
+  public boolean atSetpoint (double distance) {
+    double setpointRPM = shotSpeedTable.get(distance);
+    double setpointAngle = shotAngleTable.get(distance);
+    return
+        Math.abs(setpointRPM - getCurrentTopRollerSpeed()) &&
+        Math.abs(setpointRPM - getCurrentButtonRollerSpeed()) &&
+        Math.abs(setpointAngle - getCurrentPivotAngle()) <= toleranceAngle;
+  }//will add all of these methods next meeting - Daniel
 }
 
+
+
 // need to do main logic next meeting
+//interpolation - check 2024 robot
+//class InterpolationDoubleTreeMap
